c1ba5ebf09d98b77a6e2310e6a7f98fb0b2a9c6b
oday qsrawi <odayqsrawi@gmail.com>
2022-01-09 16:04:07 +0200

    ss

---
+++ Pre-bootcamp/LASTNAME_FIRSTNAME_JS.js
@@ -0,0 +1,21 @@
+
+//Practice one 
+function myBirthYearFunc(){
+    console.log("I was born in " + 1980);
+}
+myBirthYearFunc();
+
+//Practice two
+function myBirthYearFunc(birthYearInput){
+    console.log("I was born in " + birthYearInput);
+}
+myBirthYearFunc(2000);
+//Practice three
+function add(num1, num2){
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+add(10,30);
