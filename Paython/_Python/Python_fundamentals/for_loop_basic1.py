e5660e875147ac7cac09143b9bcb61d2395b7be5
oday qsrawi <odayqsrawi@gmail.com>
2022-01-31 20:55:58 +0200

    for_loop

---
+++ Paython/_Python/Python_fundamentals/for_loop_basic1.py
@@ -0,0 +1,24 @@
+for x in range(0,150,1):
+    print(x)
+for x in range(5,1000,+5):
+    print(x)
+for x in range(1,100,1):
+    if x % 5==0:
+        print("Coding")
+    elif x % 10==0:
+        print("coding dojo")
+    else :
+        print("nothing")
+sum = 0
+for x in range(1, 500000,2):
+    sum +=x
+print(sum)
+for x in range(2018,0,-4):
+    print(x)
+lownum=1
+highnum=9
+mult=2
+for x in range(lownum,highnum,1):
+    if x % mult == 0:
+        print(x)
+
