e9ca3d1303b789807916c9aea4fdee3c57db76b6
oday qsrawi <odayqsrawi@gmail.com>
2022-01-31 22:16:39 +0200

    functions1

---
+++ Paython/_Python/Python_fundamentals/cheking.py
@@ -0,0 +1,76 @@
+# def a():
+#     return 5
+# print(a())
+# def a():
+#     return 5
+# print(a()+a())
+# def a():
+#     return 5
+#     return 10
+# print(a())
+# def a():
+#     return 5
+#     print(10)
+# print(a())
+# def a():
+#     print(5)
+# x = a()
+# def a(b,c):
+#     return(b+c)
+# print(a(1,2) + a(2,3))
+# def a():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(a())
+# def a(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(a(2,3))
+# print(a(5,3))
+# print(a(2,3) + a(5,3))
+# def a(b,c):
+#     return b+c
+#     return 10
+# print(a(3,5))
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+# print(b)
+# a()
+# print(b)
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=a()
+# print(b)
+# def a():
+#     print(1)
+#     b()
+#     print(2)
+# def b():
+#     print(3)
+# a()
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
