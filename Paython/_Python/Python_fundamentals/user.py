01544f53bd2cc593910209dc98f878817c2bd7f0
oday qsrawi <odayqsrawi@gmail.com>
2022-02-01 20:49:48 +0200

    chining way

--- Paython/_Python/Python_fundamentals/user.py
+++ Paython/_Python/Python_fundamentals/user.py
@@ -7,11 +7,16 @@ class User:
         print(f"user:{self.name} money:{self.account_balance}")
     def make_withdrawal(self, amount):
         self.account_balance -=amount
+        return self
     def make_deposit(self, amount):
-    	self.account_balance += amount
-    
-    
-oday=User("oday","asfdoahfs")
-oday.make_deposit(100)
-print(oday.account_balance)
+        self.account_balance +=amount
+        return self
+guido=User("oday","odayqsrawi@gmail.com")
+guido.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_user_balance()
+
+
+
+
+
+
 
