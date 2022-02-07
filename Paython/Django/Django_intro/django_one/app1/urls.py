e2c57c7329d9399d4990f97a894b145ac8b21dd6
oday qsrawi <odayqsrawi@gmail.com>
2022-02-07 16:44:49 +0200

    app_one

---
+++ Paython/Django/Django_intro/django_one/app1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.root),
+    path('blogs', views.index),
+    path('blogs/new',views.new),
+    path('blogs/create', views.create),
+    path('blogs/<int:number>', views.show),
+    path('blogs/<number>/edit', views.edit),
+    path('blogs/<number>/delete', views.destroy),
+]
