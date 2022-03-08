360432b271c8261dc9e86de3cbe48fe13c5e8a03
oday qsrawi <odayqsrawi@gmail.com>
2022-03-08 17:55:26 +0200

    Barista's Challenge

--- Java/oop/OrderandItems/Item.java
+++ Java/oop/OrderandItems/Item.java
@@ -1,11 +1,27 @@
 package OrderandItems;
 public class Item {
 
-    String name;
-    double price; 
-    
+    private String name;
+    private double price; 
+    //Constructor
     public Item (String name,double price){
         this.name=name;
         this.price=price;
+    }
+    //Empty Constructor
+    public Item (){
+    }
+    //Setters And Getters
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+    public double getPrice(){
+        return this.price;
     }
 }
--- Java/oop/OrderandItems/Order.java
+++ Java/oop/OrderandItems/Order.java
@@ -3,11 +3,57 @@ package OrderandItems;
 import java.util.ArrayList;
 
 public class Order {
-    public String name;
-    public double total;
-    public boolean ready;
+    private String name;
+    private boolean ready;
     ArrayList<Item> items=new ArrayList<Item>();
+    //Empty Constructor 
+    public Order(){
+        this.name = "Guest";
+    }
+    // Constructor 
     public Order(String name){
         this.name = name;
     }
+    //Seters And getters
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setready(boolean ready){
+        this.ready = ready;
+    }
+    public boolean isReady(){
+        return ready;
+    }
+    //Actoin Methods
+    public void addItem(Item item){
+        items.add(item);
+    }
+    public String getStatusMessage(){
+        if (this.ready ==true){
+            return ("Your order is ready");
+        }
+        else{
+            return ("Thank you for waiting. Your order will be ready soon.");
+        }
+    }
+    public void display(){
+        double total = 0;
+        System.out.printf("Customer Name: %s\n", this.name);
+        for(Item item :items){
+            System.out.println(item.getName() +"  --  "+item.getPrice() +" $");
+            total =total + item.getPrice();
+        }
+        System.out.printf("Total is: %s\n", total);
+    }
+    public double getOrderTotal(){
+        double total = 0;
+        for(Item item :items){
+            total =total + item.getPrice();
+        }
+        return total;
+    }
+    
 }
