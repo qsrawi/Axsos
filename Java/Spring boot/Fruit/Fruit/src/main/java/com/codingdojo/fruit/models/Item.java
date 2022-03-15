d783dfd3a06152007ed46e842d522817185437c1
oday qsrawi <odayqsrawi@gmail.com>
2022-03-15 15:36:47 +0200

    Fruit

---
+++ Java/Spring boot/Fruit/Fruit/src/main/java/com/codingdojo/fruit/models/Item.java
@@ -0,0 +1,28 @@
+package com.codingdojo.fruit.models;
+
+public class Item {
+    private String name;
+    private double price;
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	// CONSTRUCTOR
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    } 
+}
---
+++ Java/Spring boot/Fruit/Fruit/src/main/java/com/codingdojo/fuit/FruitController.java
@@ -0,0 +1,27 @@
+package com.codingdojo.fuit;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.fruit.models.Item;
+
+@Controller
+public class FruitController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        ArrayList<Item> fruits = new ArrayList<Item>();
+        fruits.add(new Item("Kiwi", 1.5));
+        fruits.add(new Item("Mango", 2.0));
+        fruits.add(new Item("Goji Berries", 4.0));
+        fruits.add(new Item("Guava", .75));
+        
+        
+        model.addAttribute("allFruits", fruits);
+        
+        return "index.jsp";
+}
+}
