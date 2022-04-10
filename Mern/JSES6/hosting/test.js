276723fb0e11c590359ff897dd8b36716e2ee2fe
oday qsrawi <odayqsrawi@gmail.com>
2022-04-10 14:24:13 +0300

    hosting

---
+++ Mern/JSES6/hosting/test.js
@@ -0,0 +1,27 @@
+console.log(hello);                                   
+var hello = 'world'; 
+
+
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
