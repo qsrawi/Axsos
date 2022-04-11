57eff5c87639974bb2cff644b02dd244b2269734
oday qsrawi <odayqsrawi@gmail.com>
2022-04-12 01:43:12 +0300

    first with jsx

---
+++ Mern/React/with jsx/firstjsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>hello Dojo</h1>
+      <h3>Things I need to do</h3>
+      <ul>
+        <li>Lear react</li>
+        <li>Lear java</li>
+        <li>Lear python</li>
+        <li>eat apple</li>
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
