85dbc4774932dac6c86dd3746219f036fd7e1669
oday qsrawi <odayqsrawi@gmail.com>
2022-04-12 16:38:45 +0300

    props

---
+++ Mern/React/props/app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+function App() {
+  return (
+    <div className="App">
+
+        <PersonCard firstName={"oday"} lastName={"qsrawi"} age={"23"} hairColor={"red"}/>
+        <PersonCard firstName={"oday"} lastName={"qsrawi"} age={"23"} hairColor={"red"}/>
+        <PersonCard firstName={"oday"} lastName={"qsrawi"} age={"23"} hairColor={"red"}/>
+        <PersonCard firstName={"oday"} lastName={"qsrawi"} age={"23"} hairColor={"red"}/>
+
+    </div>
+  );
+}
+
+export default App;
